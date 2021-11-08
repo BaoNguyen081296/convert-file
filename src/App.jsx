d1c521c533f0c255dcf43a2d22332e97c3001820
Bao Nguyen Hoai <baonguyen081296@gmail.com>
2021-11-08 18:15:32 +0700

    structure app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Convert from './pages/Convert';
+import './App.scss';
+export default function App() {
+  return (
+    <div className="app">
+      <Convert />
+    </div>
+  );
+}
---
+++ src/pages/Convert/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Convert.scss';
+export default function index() {
+  return (
+    <div className="_convert">
+      <h1>Convert file</h1>
+    </div>
+  );
+}
